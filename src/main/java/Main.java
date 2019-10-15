348be22c9f56239517dee391ecf6b9ef812bb14c
847001315 <49674990+847001315@users.noreply.github.com>
2019-10-15 21:39:32 +0800

    删除没用的包

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
