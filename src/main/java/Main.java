89910cf61f331be0c10c74e7cf510da9848bc736
847001315 <49674990+847001315@users.noreply.github.com>
2019-10-22 21:20:42 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -14,38 +16,34 @@ public class Main {
     // //初步设定
     //        //五毒 0  炸弹 1
     //        //葫芦 2  同花 3  顺子 4  三条 5  多对6 单对7
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws InterruptedException {
 
         /*Main
             先执行 那个登陆的情况。如果可以的话，再去执行其它的
             得到一个字符串。
             * */
         /*-----------------------------------*/
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor(System.out::println);
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.shisanshui.rtxux.xyz/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         API request = retrofit.create(API.class);
-        login k = new login("mytest02","test8");
+        login k = new login("847001315","cgh1314520wjm,.");
+        //login k = new login("031702502","123456");
         //这上面的基本不用改；
+
+
+
+
         String token="";
 
-        //final String[] token = {" "};
-//        call.enqueue(new Callback<login_return>() {
-//            @Override
-//            public void onResponse(Call<login_return> call, Response<login_return> response) {
-//               // token[0] =response.body().getData().token;
-//                        System.out.println(response.body().getStatus());
-//                        System.out.println(response.body().getData().user_id);
-//                        System.out.println(response.body().getData().token);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<login_return> call, Throwable throwable) {
-//                System.out.println("gg");
-//            }
-//        });
-//        final String[] ghhh = new String[1];
         try {
             Call<login_return> call = request.getCall(k);
             Response<login_return> response = call.execute();
@@ -53,13 +51,14 @@ public class Main {
             String result = response.body().getData().token;
             //ghhh[0] =result;
             token=result;
-            System.out.println(result);
-            System.out.println(response.body().getData().user_id);
+            System.out.println("测试得到的验证码："+result);
+            System.out.println("测试得到的id号： "+response.body().getData().user_id);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for(int gggg=0;gggg<200001;gggg++){
+            System.out.println("测试第"+gggg+"次");
         String card="";
-        System.out.println("好像失败了");
         int id=0;
         try {
             Call<open_return> call = request.getCall(token);
@@ -69,8 +68,8 @@ public class Main {
         }catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(card);
-        System.out.println(id);
+        System.out.println("测试得到的牌:"+card);
+        System.out.println("测试得到的牌局ID"+id);
 
 
         /*-----------------------------------*/
@@ -78,10 +77,11 @@ public class Main {
         int m[];
         m = new int[10];
         String a=card;
-        //String a ="#A #2 #3 #4 #5 #6 #7 #8 #9 #10 #J #Q #K";
+        //String a ="#10 #K &7 *10 &K #2 #J &4 &2 &6 #5 #7 #4";
         //"$2$3$4$5$6*5$Q#8&8*A*8&3*3"
         //"$2&2$8&5$10*5$Q#8&8*A*8&3*3"
         //"$2$3$4$A$6&8*2*5*A*8*3&3#8"
+            //#A #3 #2 &4 &2 *2 $2 #J &Q #4 $A *3 &J
         SSS K = new SSS(a,1);
         K.returnkmk(m);//判断后墩可能有哪些组合
 
@@ -107,8 +107,8 @@ public class Main {
             }
         }
 
-        zongti[max_i].printwai();
-        Puke puke=new Puke(zongti[max_i].result[max_j],id);
+       //zongti[max_i].printwai();
+       Puke puke=new Puke(zongti[max_i].result[max_j],id);
         //这边传回不同的类型组合/。
         //然后这边再判断。
         String msg="";
@@ -116,19 +116,19 @@ public class Main {
         try {
             Call<Submit_return> call = request.getCall(token,puke);
             Response<Submit_return> response = call.execute();
-            msg=response.body().getData().msg;
+            //msg=response.body().getData().msg;
         }catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(msg);
+        //System.out.println("测试的成果："+msg);
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
         String jsonObject = gson.toJson(puke);
         System.out.println(jsonObject);
-        System.out.println(puke.card[0]);
+        System.out.println();
 
 
     }
 
-
+    }
 }
